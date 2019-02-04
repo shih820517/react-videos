4a76ba743a7136876c5176c359ea296ab7e8725f
Huai-Chen <shih820517@gmail.com>
2019-02-03 17:53:27 -0800

    complete react project: videos

---
+++ src/api/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import KEY from 'KEY.js';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResult: 5,
+    key: KEY
+  }
+});
